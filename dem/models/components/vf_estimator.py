f99a4b4e94730680d4b72e2447e8849909038645
dongyeop3813 <dywoo1247@gmail.com>
2024-05-01 11:17:40 +0900

    Implement VE path

--- dem/models/components/vf_estimator.py
+++ dem/models/components/vf_estimator.py
@@ -6,9 +6,35 @@ from dem.models.components.clipper import Clipper
 from dem.models.components.noise_schedules import BaseNoiseSchedule
 
 
+DEBUG = False
+
+
+def VE_conditional_VF(
+    perturbed: torch.Tensor,
+    endpoint: torch.Tensor,
+    time: torch.Tensor,
+    noise_schedule: BaseNoiseSchedule,
+):
+    """
+    VE conditioanl vector field.
+    For given time points and origin, 
+    evaluate CVF at given points (perturbed).
+
+    Here K = sample size, D = dimension of data.
+    
+    :param perturbed: Points where CVF will be evaluated. (D dimension)
+    :param endpoint: Points where transport ends. (K x D dimension)
+    :param time: time step t. (scalar)
+
+    :return: Evaluted (conditional) vector field (K x D dimension)
+    """
+    c = torch.log(torch.tensor(noise_schedule.sigma_diff).to(perturbed))
+    return c * (perturbed - endpoint)
+
+
 def OT_conditional_VF(
     perturbed: torch.Tensor, 
-    origin: torch.Tensor,
+    endpoint: torch.Tensor,
     time: torch.Tensor,
     noise_schedule: BaseNoiseSchedule,
 ):
@@ -20,27 +46,43 @@ def OT_conditional_VF(
     Here K = sample size, D = dimension of data.
     
     :param perturbed: Points where CVF will be evaluated. (D dimension)
-    :param origin: Points where move starts. (K x D dimension)
+    :param endpoint: Points where transport ends. (K x D dimension)
     :param time: time step t. (scalar)
 
     :return: Evaluted (conditional) vector field (K x D dimension)
     """
 
-    coeff = 1 - noise_schedule.sigma_min
-    return (origin - coeff * perturbed) / (1 - coeff * time + 1e-5)
+    coeff = 1 - noise_schedule.sigma_diff
+    return (endpoint - coeff * perturbed) / (1 - coeff * time + 1e-23)
 
 
-def VF_estimator(
+def sampling_from_OT_importance(t, x, sigma_t, device):
+    return (
+        (x + (torch.randn_like(x, device=device) * sigma_t.sqrt())) 
+        /
+        (t.unsqueeze(1) + 1e-23)
+    )
+
+
+def sampling_from_VE_importance(t, x, sigma_t, device):
+    return (
+        x + torch.randn_like(x, device=device) * sigma_t.sqrt()
+    )
+
+
+def _VF_estimator(
     t: torch.Tensor,
     x: torch.Tensor,
     energy_function: BaseEnergyFunction,
     noise_schedule: BaseNoiseSchedule,
     num_mc_samples: int,
-    option: str = "OT CVF",
+    option: str = "OT",
+    device="cpu",
 ) -> torch.Tensor:
     """
+    Estimate marginal vector field with signle time, x input.
     Here D = dimension of data.
-    
+
     :param t: timestep (scalar)
     :param x: data (D dimension)
 
@@ -49,28 +91,48 @@ def VF_estimator(
 
     # energy_function = -E(x)
 
+    # choose probabilith path
+    if option == "OT":
+        conditional_VF = OT_conditional_VF
+        sample_from_importance_dist = sampling_from_OT_importance
+
+    elif option == "VE":
+        conditional_VF = VE_conditional_VF
+        sample_from_importance_dist = sampling_from_VE_importance
+
     repeated_t = t.unsqueeze(0).repeat_interleave(num_mc_samples, dim=0)
     repeated_x = x.unsqueeze(0).repeat_interleave(num_mc_samples, dim=0)
 
     h_t = noise_schedule.h(repeated_t).unsqueeze(1)
 
-    # K x D dimension
-    samples = (repeated_x + (torch.randn_like(repeated_x) * h_t.sqrt())) / (repeated_t.unsqueeze(1) + 1e-5)
+    # sample from importance distribution q(-;x) (K x D dimension)
+    endpoint_candidates = sample_from_importance_dist(
+        repeated_t,
+        repeated_x,
+        h_t,
+        device=device,
+    )
 
     # log (unnormalized) prob of each sample (dimension K)
-    log_prob = energy_function(samples)
-
-    # choose probabilith path
-    if option == "OT CVF":
-        conditional_VF = OT_conditional_VF
-    else:
-        raise Exception("Invalid probability path")
+    log_prob = energy_function(endpoint_candidates)
 
     # weights (dimension K)
     weights = torch.softmax(log_prob, dim=0)
 
+    cvf = conditional_VF(x, endpoint_candidates, t, noise_schedule)
+
+    if DEBUG:
+        print(endpoint_candidates)
+        print(f"endpoint has nan {endpoint_candidates.isnan().max()}")
+        print(f"weights has nan {weights.isnan().max()}")
+        print(f"cvf has nan {cvf.isnan().max()}")
+        print(cvf)
+
     # marginal vector field estimator
-    return torch.matmul(weights, conditional_VF(x, samples, t, noise_schedule))
+    return torch.matmul(
+        weights, 
+        cvf
+    )
 
 
 def estimate_VF(
@@ -79,50 +141,46 @@ def estimate_VF(
     energy_function: BaseEnergyFunction,
     noise_schedule: BaseNoiseSchedule,
     num_mc_samples: int,
-    option: str = "OT CVF",
+    option: str = "OT",
+    device="cpu",
 ):
+    """
+        Estimate marginal vector field with batched time, x input.
+    """
 
     # vectorizing map for batch processing
-    vmmaped_fxn = torch.vmap(VF_estimator, in_dims=(0, 0, None, None, None, None), randomness="different")
-
-    return vmmaped_fxn(t, x, energy_function, noise_schedule, num_mc_samples, option)
-
-
-if __name__ == "__main__":
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from dem.models.components.noise_schedules import OTcnfNoiseSchedule
-    from dem.energies.gmm_energy import GMM
-    from fab.utils.plotting import plot_contours
-    from matplotlib.animation import FuncAnimation
-
-    noise_schedule = OTcnfNoiseSchedule(0.001)
-    energy_function = GMM()
-    grid_steps = 20
-    batch_size = grid_steps ** 2
-    plotting_bounds = (-1.4 * 40, 1.4 * 40)
-
-    x_coord, y_coord = np.meshgrid(
-        np.linspace(*plotting_bounds, grid_steps), 
-        np.linspace(*plotting_bounds, grid_steps)
+    vmmaped_fxn = torch.vmap(
+        _VF_estimator, 
+        in_dims=(0, 0, None, None, None, None), 
+        randomness="different"
     )
-    x = torch.tensor([x_coord, y_coord], dtype=torch.float).T.flatten(start_dim=0, end_dim=1)
 
-    for time in np.linspace(0, 1, 10):
-        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-        plot_contours(
-            energy_function,
-            bounds=plotting_bounds,
-            ax = ax,
-            n_contour_levels=50,
-            grid_width_n_points=200,
-        )
+    return vmmaped_fxn(
+        t, x, energy_function, noise_schedule, num_mc_samples, 
+        option, device=device
+    )
 
-        t = torch.tensor([time * 0.1] * batch_size, dtype=torch.float)
-        estimated_VF = estimate_VF(t, x, energy_function, noise_schedule, 1000).detach()
 
-        ax.quiver(x_coord, y_coord, estimated_VF[:,0], estimated_VF[:,1])
-        fig.savefig(f"./gmm_vf_time={time}.png")
+def estimate_VF_for_fixed_time(
+    t: torch.Tensor,
+    x: torch.Tensor,
+    energy_function: BaseEnergyFunction,
+    noise_schedule: BaseNoiseSchedule,
+    num_mc_samples: int,
+    option: str = "OT",
+    device="cpu",
+):
+    """
+        Estimate marginal vector field with batched x and single time input.
+    """
 
-    # ani = FuncAnimation(fig, animate, 10, repeat=False)
-    # ani.save("./gmm_vf.mp4")
+    vmmaped_fxn = torch.vmap(
+        _VF_estimator,
+        in_dims=(None, 0, None, None, None, None),
+        randomness="different"
+    )
+
+    return vmmaped_fxn(
+        t, x, energy_function, noise_schedule, num_mc_samples,
+        option, device=device
+    )
